e5a969466a316b94e8010b926d27c103745e86f7
Wendell <dellhribeiro@gmail.com>
2023-09-05 03:47:42 -0300

    initial commit

---
+++ src/test/java/me/dio/SantanderAPI/REST/SantanderApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.SantanderAPI.REST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
